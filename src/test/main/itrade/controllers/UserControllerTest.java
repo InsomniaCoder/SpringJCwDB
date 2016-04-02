7fecae3af77a99cf367d8227279476e18e08fc3b
tanat lokejaroenlarb <tanatloke@gmail.com>
2016-04-02 10:52:32 +0700

    remove unfinished controller test

--- src/test/main/itrade/controllers/UserControllerTest.java
+++ src/test/main/itrade/controllers/UserControllerTest.java
@@ -1,32 +1,11 @@
 package test.main.itrade.controllers;
 
-import main.itrade.ApplicationConfigure;
-import main.itrade.data.entities.User;
-import main.itrade.data.repositories.UserRepository;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 /**
  * Created by tanatlokejaroenlarb on 4/1/2016 AD.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {ApplicationConfigure.class})
-@WebAppConfiguration
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = {ApplicationConfigure.class})
 public class UserControllerTest {
 
-    //main entry point, execute request by calling perform
-    private MockMvc mockMvc;
-    //Mock request by MockMVCRequestBuilder
-
-    UserRepository userRepository;
-
-    @Test
-    public void login_withAdminUserNameAndPasswordShouldReturn200(){
-
-        User adminUser = User.createUser(User.UserRole.ADMIN,"admin","admin");
-    }
 
 }
