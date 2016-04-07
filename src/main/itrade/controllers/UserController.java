0582c60364ecc53ea9791dd08644a40f01853245
Tanat Lokejaroenlarb <tanatloke@gmail.com>
2016-04-07 11:14:57 +0700

    update version maven spring to 4.2 for using @CrossOrigin

--- src/main/itrade/controllers/UserController.java
+++ src/main/itrade/controllers/UserController.java
@@ -6,10 +6,7 @@ import main.itrade.data.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +22,7 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @RequestMapping(value = "/login",method = RequestMethod.POST,consumes ="application/json")
     public ResponseEntity<String> login(@RequestBody String jsonUser){
 
