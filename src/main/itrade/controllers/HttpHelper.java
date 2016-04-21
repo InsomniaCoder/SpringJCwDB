604ee5dfa3266dab3a0630470363ff932d3b9eb1
tanat lokejaroenlarb <tanatloke@gmail.com>
2016-04-21 14:47:23 +0700

    add check session method and move build header into HttpHeader

--- src/main/itrade/controllers/HttpHelper.java
+++ src/main/itrade/controllers/HttpHelper.java
@@ -1,7 +1,31 @@
 package main.itrade.controllers;
 
+import org.springframework.http.HttpHeaders;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 /**
  * Created by tanatlokejaroenlarb on 4/21/2016 AD.
  */
 public class HttpHelper {
+
+    public static boolean sessionIsExist(HttpServletRequest request){
+
+        HttpSession session = request.getSession(false);
+        if(session == null){
+            return false;
+        }else{
+            return  true;
+        }
+    }
+
+    public static HttpHeaders buildHeader(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json; charset=UTF-8");
+        headers.add("Access-Control-Allow-Origin", "*");
+        return headers;
+    }
+
+
 }
