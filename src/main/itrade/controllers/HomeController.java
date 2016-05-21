26ca94bea67ee2f9f90bf7d3405ef2f0a30622ed
InsomniaCoder <InsomniaCoder>
2016-05-22 01:08:05 +0700

    add test controller for giving model and view

--- src/main/itrade/controllers/HomeController.java
+++ src/main/itrade/controllers/HomeController.java
@@ -1,7 +1,22 @@
 package main.itrade.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * Created by CartoON on 5/22/2016.
  */
+@Controller
 public class HomeController {
+
+    
+         @RequestMapping(value = { "/home"}, method = RequestMethod.GET)
+         public ModelAndView welcomePage() {
+                 ModelAndView modelAndView = new ModelAndView();
+                 modelAndView.setViewName("index");
+                 return modelAndView;
+             }
+
 }
