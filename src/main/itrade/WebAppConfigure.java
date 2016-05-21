8e54c737eb9f3d063dff2f99d8e9b40c6fc80258
InsomniaCoder <InsomniaCoder>
2016-05-22 01:07:33 +0700

    Add view resolver to web app configure( spring dispatcher configure)

--- src/main/itrade/WebAppConfigure.java
+++ src/main/itrade/WebAppConfigure.java
@@ -1,8 +1,14 @@
 package main.itrade;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 /**
  * Created by tanatlokejaroenlarb on 3/31/2016 AD.
@@ -11,4 +17,14 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @ComponentScan(basePackages = "main.itrade.controllers")
 public class WebAppConfigure {
-}
+
+         @Bean
+         public InternalResourceViewResolver viewResolver() {
+             InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+             viewResolver.setViewClass(JstlView.class);
+             viewResolver.setPrefix("/WEB-INF/");
+             viewResolver.setSuffix(".jsp");
+             return viewResolver;
+         }
+
+    }
